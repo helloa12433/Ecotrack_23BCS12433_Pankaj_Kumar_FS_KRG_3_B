2f936aaa8d70f7cf65be3a2237c4684f505f3198
Pankaj Kumar <pankajad21@gmail.com>
2026-02-04 17:22:36 +0000

    Added experiment folders

---
+++ experiment 01/code/src/App.jsx
@@ -0,0 +1,11 @@
+import EcoTrack from "./EcoTrack.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <EcoTrack />
+    </div>
+  );
+};
+
+export default App;
---
+++ experiment 01/code/src/logs.js
@@ -0,0 +1,6 @@
+export const logs = [
+  { id: 1, activity: "Car Travel", carbon: 50},
+  { id: 2, activity: "Electricity Usage", carbon: 60},
+  { id: 3, activity: "Cycling", carbon: 0 },
+  {id :4,activity:"Coal",carbon:400},
+];
